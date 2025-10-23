b15e7250db198693302ce63b68669f2098b0523e
Oz-Abohazira <oz.abohazira@rebillia.com>
2025-10-23 08:48:30 -0400

    Initial commit

---
+++ src/components/ServiceMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { servicesData } from '../assets/assets'
+
+const ServiceMenu = () => {
+  return (
+    <div id="service-type">
+        <h1>Find the Right Service for You</h1>
+        <p>Take a look through our wide selection of services.<br />
+           Whether you've got something in mind or need a little guidance, we'll help you decide with confidence.</p>
+        
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ServiceMenu
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import ServiceMenu from '../components/ServiceMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <ServiceMenu />
+    </div>
+  )
+}
+
+export default Home
