75478af6d0df85b9587654a50dc1d72cde46b16d
Oz-Abohazira <oz.abohazira@rebillia.com>
2025-10-27 13:08:14 -0400

    Removing console.log

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
     const [token, setToken] = useState(true);
     const [showDropdown, setShowDropdown] = useState(false);
 
+    const hrStyle = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden';
+
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (showDropdown && !event.target.closest('.group')) {
@@ -28,23 +30,23 @@ const Navbar = () => {
             <ul className='hidden md:flex items-start gap-5 font-medium'>
                 <NavLink to='/'>
                     <li className='py-1'>HOME</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className={hrStyle} />
                 </NavLink>
                 <NavLink to='/services'>
                     <li className='py-1'>ALL SERVICES</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className={hrStyle} />
                 </NavLink>
                 <NavLink to='/about'>
                     <li className='py-1'>ABOUT</li>
-                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+                    <hr className={hrStyle} />
                 </NavLink>
             </ul>
             <div className='flex items-center gap-4'>
                 {
                     token
                         ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-                            <img onClick={() => { setShowDropdown(!showDropdown); console.log(showDropdown)}} className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-                            <img onClick={() => { setShowDropdown(!showDropdown); console.log(showDropdown)}} className='w-2.5' src={assets.dropdown_icon} alt="" />
+                            <img onClick={() => setShowDropdown(!showDropdown)} className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                            <img onClick={() => setShowDropdown(!showDropdown)} className='w-2.5' src={assets.dropdown_icon} alt="" />
                             <div className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 ${showDropdown ? 'block' : 'hidden'} group-hover:block`}>
                                 <div className='min-w-44 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                                     <p className='hover:text-black cursor-pointer' onClick={() => navigate('/my-profile')}>My Profile</p>
